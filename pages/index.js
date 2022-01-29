d1ae25dd1f4853d3202abed0714b2db188de408b
ankit <ankitpathak143192@gmail.com>
2022-01-29 15:23:27 +0530

    hotfix

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React,{useEffect} from "react"
 import { useRouter } from "next/router";
 import { useWallet } from "@solana/wallet-adapter-react";
 import NotConnected from "../components/Dashboard/NotConnected";
