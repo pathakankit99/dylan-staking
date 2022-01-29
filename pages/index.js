b79cc22696d6bbb27d344e30bcdf4f2432b389f6
ankit <ankitpathak143192@gmail.com>
2022-01-29 15:27:16 +0530

    hotfix

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,15 @@ import NotConnected from "../components/Dashboard/NotConnected";
 export default function Home() {
   const { publicKey } = useWallet();
   const router = useRouter();
-  useEffect(() => {
-    if (!publicKey) {
-      return (
-        <div className="center h-80vh">
-          <NotConnected />
-        </div>
-      );
-    } else {
-      router.push("/dashboard");
-    }
-  }, [publicKey]);
+   if (!publicKey) {
+     return (
+       <div className="center h-80vh">
+         <NotConnected />
+       </div>
+     );
+   } else {
+     router.push("/dashboard");
+   }
 
   return <></>;
 }
